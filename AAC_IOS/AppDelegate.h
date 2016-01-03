d0de927996b3ac14dc3691056ce78ceb587044a7
Jason Liu <happyorange8868@163.com>
2016-01-03 15:02:55 +1000

    Initial Commit

---
+++ AAC_IOS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AAC_IOS
+//
+//  Created by Jason_Liu on 3/01/2016.
+//  Copyright © 2016 LiuJason. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AAC_IOS/SecondViewController.h
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.h
+//  AAC_IOS
+//
+//  Created by Jason_Liu on 3/01/2016.
+//  Copyright © 2016 LiuJason. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+
+@end
+
